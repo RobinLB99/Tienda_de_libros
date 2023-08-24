add333766bd2f10fa7916c5ab9fdfb9ced67b8b2
RobinLB99 <rjlb991102@hotmail.com>
2023-08-24 11:13:29 -0500

    Reorganización del arbol de archivos

---
+++ src/main/frontend/src/js/datatables.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+import DataTable from "datatables.net-bs5";
+import "datatables.net-buttons-bs5";
+import "datatables.net-buttons/js/buttons.colVis.mjs";
+import "datatables.net-buttons/js/buttons.html5.mjs";
+import "datatables.net-colreorder-bs5";
+import "datatables.net-keytable-bs5";
+import "datatables.net-responsive-bs5";
+import "datatables.net-scroller-bs5";
+import "datatables.net-searchpanes-bs5";
+
+let tablaLibros = new DataTable("#TablaLibros");
