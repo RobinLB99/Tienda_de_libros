fcb92f67b616c6f9ad091503e8a63d0efe838f5c
RobinLB99 <rjlb991102@hotmail.com>
2023-08-25 13:41:47 -0500

    Servicio de ingreso de empleado

---
+++ src/main/java/Servellets/ServelletsEmpleados/SvCrearEmpleado.java
@@ -0,0 +1,90 @@
+package Servellets.ServelletsEmpleados;
+
+import Logica.ConvertirFecha;
+import Logica.Empleado;
+import Logica.LogicController;
+import Logica.Usuario;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ *
+ * @author joel
+ */
+@WebServlet(name = "SvCrearEmpleado", urlPatterns = {"/SvCrearEmpleado"})
+public class SvCrearEmpleado extends HttpServlet {
+    
+    LogicController control = null;
+    Empleado empleado = null;
+    Usuario user = null;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+        try {
+            String cedula = request.getParameter("cedula_");
+            String nombres = request.getParameter("nombres_");
+            String apellidos = request.getParameter("apellidos_");
+            String nacimiento = request.getParameter("fechaNacimiento_");
+            String telefono = request.getParameter("telefono_");
+            String cargo = request.getParameter("cargo_");
+
+            /* Convertir a tipo Date */
+            ConvertirFecha convert = new ConvertirFecha(nacimiento);
+            Date fechaNacimiento = convert.getDate();
+            
+            try {
+                control = new LogicController();
+                empleado = new Empleado();
+                user = new Usuario();
+                
+                control.crearUsuario(user);
+                
+                empleado.setCedula(cedula);
+                empleado.setNombre(nombres);
+                empleado.setApellidos(apellidos);
+                empleado.setFechaNacimiento(fechaNacimiento);
+                empleado.setTelefono(telefono);
+                empleado.setFuncion(cargo);
+                empleado.setUsuario(user);
+                
+                control.crearEmpleado(empleado);
+                
+                response.sendRedirect("index.jsp");
+                
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                response.sendRedirect("index.jsp?accion=error");
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.sendRedirect("aniadirEmpleado.jsp"); /*Añadir mensaje por url*/
+        }
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
