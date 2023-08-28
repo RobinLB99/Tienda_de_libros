e24d04269df9eb4f70015e1d873199b73c9f7704
RobinLB99 <rjlb991102@hotmail.com>
2023-08-28 10:53:26 -0500

    Refactorización para los controlles de persistencia de la Clase Acceso

--- src/main/java/Persistencia/PersistenceController.java
+++ src/main/java/Persistencia/PersistenceController.java
@@ -2,23 +2,23 @@ package Persistencia;
 
 import Logica.Empleado;
 import Logica.Libro;
-import Logica.Usuario;
+import Logica.Acceso;
 import Persistencia.exceptions.NonexistentEntityException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PersistenceController {
-    
+
     EmpleadoJpaController employJpa = new EmpleadoJpaController();
-    UsuarioJpaController userJpa = new UsuarioJpaController();
+    AccesoJpaController userJpa = new AccesoJpaController();
     LibroJpaController bookJpa = new LibroJpaController();
-    
+
     /* Libros */
     public void crearLibro(Libro libro) {
         bookJpa.create(libro);
     }
-    
+
     public void editarLibro(Libro libro) {
         try {
             bookJpa.edit(libro);
@@ -26,28 +26,28 @@ public class PersistenceController {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void eliminarLibro(long id) {
         try {
             bookJpa.destroy(id);
         } catch (NonexistentEntityException ex) {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
-    } 
-    
+    }
+
     public Libro buscarLibro(long id) {
         return bookJpa.findLibro(id);
     }
-    
+
     public List<Libro> listaLibros() {
         return bookJpa.findLibroEntities();
     }
-    
+
     /* Empleados */
     public void crearEmpleado(Empleado employ) {
         employJpa.create(employ);
     }
-    
+
     public void editarEmpleado(Empleado empleado) {
         try {
             employJpa.edit(empleado);
@@ -55,7 +55,7 @@ public class PersistenceController {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void eliminarEmpleado(long id) {
         try {
             employJpa.destroy(id);
@@ -63,42 +63,42 @@ public class PersistenceController {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public Empleado buscarEmpleado(long id) {
         return employJpa.findEmpleado(id);
     }
-    
+
     public List listaEmpleados() {
         return employJpa.findEmpleadoEntities();
     }
-    
+
     /* Usuarios */
-    public void crearUsuario(Usuario user) {
+    public void crearAcceso(Acceso user) {
         userJpa.create(user);
     }
-    
-    public void editarUsuario(Usuario user) {
+
+    public void editarAcceso(Acceso user) {
         try {
             userJpa.edit(user);
         } catch (Exception ex) {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void eliminarUsuario(long id) {
+
+    public void eliminarAcceso(long id) {
         try {
             userJpa.destroy(id);
         } catch (NonexistentEntityException ex) {
             Logger.getLogger(PersistenceController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public Usuario buscarUsuario(long id) {
-        return userJpa.findUsuario(id);
+
+    public Acceso buscarAcceso(long id) {
+        return userJpa.findAcceso(id);
     }
-    
-    public List listaUsuarios() {
-        return userJpa.findUsuarioEntities();
+
+    public List listaAccesos() {
+        return userJpa.findAccesoEntities();
     }
-    
+
 }
