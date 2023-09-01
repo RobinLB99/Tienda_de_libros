a5ef0e7b2b38335dc4791310269d20cb88fe49de
RobinLB99 <rjlb991102@hotmail.com>
2023-09-01 11:29:50 -0500

    Nuevo Servellet de cambio de privilegios

---
+++ src/main/java/Servellets/SvsCredenciales/SvChangePrivileges.java
@@ -0,0 +1,79 @@
+package Servellets.SvsCredenciales;
+
+import Logica.Acceso;
+import Logica.LogicController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author joel
+ */
+@WebServlet(name = "SvChangePrivileges", urlPatterns = {"/SvChangePrivileges"})
+public class SvChangePrivileges extends HttpServlet {
+    
+    LogicController control = null;
+    Acceso credencial = null;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        long id = Long.parseLong((String) request.getParameter("id_"));
+        String permiso = (String) request.getParameter("permisos_");
+        
+        boolean isAdmin = false; 
+        
+        try {
+            control = new LogicController();
+            
+            credencial = control.buscarAcceso(id);
+            
+            if (permiso.equals("admin")) {
+                isAdmin = true;
+            }
+           
+            credencial.setAdmin(isAdmin);
+            
+            control.editarAcceso(credencial);
+            
+            String actionString = "privilegios";
+            HttpSession accion = request.getSession();
+            accion.setAttribute("accion", actionString);
+            
+            try {
+                control.redirectToServellet(request, response, "/SvRedirigirGestionEmpleados");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                response.sendError(400);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.sendRedirect("error500.jsp");
+        }
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
