b74727d0884ddf2324b9e43a33c71ea5d047964c
RobinLB99 <rjlb991102@hotmail.com>
2023-09-03 14:07:36 -0500

    Eliminación de importaciones innecesarias

--- src/main/java/Servellets/SvsCredenciales/SvEliminarCredencial.java
+++ src/main/java/Servellets/SvsCredenciales/SvEliminarCredencial.java
@@ -2,15 +2,12 @@ package Servellets.SvsCredenciales;
 
 import Logica.Acceso;
 import Logica.LogicController;
-import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 /**
  *
