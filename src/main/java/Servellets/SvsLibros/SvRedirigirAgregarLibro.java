812c519eb0b1074e5b7efd302877e1b69719c16d
RobinLB99 <rjlb991102@hotmail.com>
2023-09-03 14:06:55 -0500

    Correción de declaración de paquete

--- src/main/java/Servellets/SvsLibros/SvRedirigirAgregarLibro.java
+++ src/main/java/Servellets/SvsLibros/SvRedirigirAgregarLibro.java
@@ -1,7 +1,6 @@
-package Servellets;
+package Servellets.SvsLibros;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
