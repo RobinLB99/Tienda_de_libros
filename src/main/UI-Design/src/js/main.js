6a05f43ac252b9718ead77190c57e194c5b19df0
RobinLB99 <rjlb991102@hotmail.com>
2023-09-04 16:18:47 -0500

    .

--- src/main/UI-Design/src/js/main.js
+++ src/main/UI-Design/src/js/main.js
@@ -17,3 +17,4 @@ import "./modalesGestionUsuario";
 import "./modalRecuperarPassword";
 import "./modalesLogin";
 import "./validacionesGestionCredenciales";
+import "./retriveValuesFromTablesRecord";
