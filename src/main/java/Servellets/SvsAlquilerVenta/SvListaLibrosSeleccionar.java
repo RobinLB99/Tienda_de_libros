739553d820f0a9a73242b7e8821655c4479be2f2
RobinLB99 <rjlb991102@hotmail.com>
2023-09-04 17:41:31 -0500

    Nuevo Servellet enlistar los libros a seleccionar

---
+++ src/main/java/Servellets/SvsAlquilerVenta/SvListaLibrosSeleccionar.java
@@ -0,0 +1,57 @@
+package Servellets.SvsAlquilerVenta;
+
+import Logica.Libro;
+import Logica.LogicController;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ *
+ * @author joel
+ */
+@WebServlet(name = "SvListaLibrosSeleccionar", urlPatterns = {"/SvListaLibrosSeleccionar"})
+public class SvListaLibrosSeleccionar extends HttpServlet {
+    
+    LogicController control = null;
+    List<Libro> listaLibros = null;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            control = new LogicController();
+            listaLibros = control.listaLibros();
+            
+            HttpSession listaLibrosSelect = request.getSession();
+            listaLibrosSelect.setAttribute("listaLibrosSelect", listaLibros);
+            
+            response.sendRedirect("SeleccionarLibro.jsp");
+            
+        } catch (Exception e) {
+            response.sendRedirect("error500.jsp");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
