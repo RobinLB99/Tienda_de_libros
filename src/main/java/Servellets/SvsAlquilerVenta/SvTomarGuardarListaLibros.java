a2834358ec565068b4a51962ec30bb2a4684ecbb
RobinLB99 <rjlb991102@hotmail.com>
2023-09-05 16:18:17 -0500

    Toma el o los libros seleccionados y trae la lista de clientes

---
+++ src/main/java/Servellets/SvsAlquilerVenta/SvTomarGuardarListaLibros.java
@@ -0,0 +1,60 @@
+package Servellets.SvsAlquilerVenta;
+
+import Logica.Cliente;
+import Logica.LogicController;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author joel
+ */
+@WebServlet(name = "SvTomarGuardarListaLibros", urlPatterns = {"/SvTomarGuardarListaLibros"})
+public class SvTomarGuardarListaLibros extends HttpServlet {
+    
+    LogicController control = null;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String stringJSON = (String) request.getParameter("arrayObjetos_");
+        
+        HttpSession JSON = request.getSession();
+        JSON.setAttribute("librosSeleccionados", JSON);
+
+        try {
+            List<Cliente> clientes = new ArrayList<Cliente>();
+            
+            HttpSession listaCLientes = request.getSession();
+            listaCLientes.setAttribute("listaCLientes", clientes);
+            
+            response.sendRedirect("DatosTransaccion.jsp");
+            
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
