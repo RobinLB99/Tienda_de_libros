62cccc7b01315d09beded309cff6af1ec0adf39d
RobinLB99 <rjlb991102@hotmail.com>
2023-09-08 15:29:32 -0500

    Servellet para el registro de facturas de alquiler

---
+++ src/main/java/Servellets/SvsAlquilerVenta/SvIngresarFacturaAlquiler.java
@@ -0,0 +1,84 @@
+package Servellets.SvsAlquilerVenta;
+
+import Logica.Acceso;
+import Logica.Alquiler;
+import Logica.CantidadLibroPedido;
+import Logica.Cliente;
+import Logica.Empleado;
+import Logica.LogicController;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author joel
+ */
+@WebServlet(name = "SvIngresarFacturaAlquiler", urlPatterns = {"/SvIngresarFacturaAlquiler"})
+public class SvIngresarFacturaAlquiler extends HttpServlet {
+
+    LogicController control = new LogicController();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Cliente cliente = (Cliente) request.getSession().getAttribute("dataCliente");
+        List<CantidadLibroPedido> librosPedidos = (List<CantidadLibroPedido>) request.getSession().getAttribute("listaCantidadLibrosPedidos");
+        String numFactura = (String) request.getSession().getAttribute("numFactura");
+
+        Acceso credencial = (Acceso) request.getSession().getAttribute("credencial");
+
+        try {
+            Empleado empleado = control.getEmployByUserName((String) credencial.getUserName());
+
+            Alquiler facALquiler = new Alquiler();
+            List<CantidadLibroPedido> listPedido = new ArrayList();
+
+            for (CantidadLibroPedido pedido : librosPedidos) {
+                control.crearCantidadLibro(pedido);
+                listPedido.add(pedido);
+            }
+
+            if (cliente.getId() == 0) {
+                control.crearCliente(cliente);
+            }
+
+            facALquiler.setNumFactura(numFactura);
+            facALquiler.setCliente(cliente);
+            facALquiler.setEmpleado(empleado);
+            facALquiler.setEstadoAlquiler("Activo");
+            facALquiler.setLibrosPedidos(listPedido);
+
+            control.crearFacturaALquiler(facALquiler);
+
+            response.sendRedirect("index.jsp?accion=alquilado");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.sendRedirect("error500.jsp");
+        }
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
